41ea51757a363015c4f3cbc65d9d60edf4a5ebd4
1billy17 <minipor00@gmail.com>
2024-12-27 07:48:50 +0300

    nice listbox

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,15 +1,75 @@
+using System;
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using demo3.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Avalonia.Layout;
 
 namespace demo3;
 
 public partial class MainWindow : Window
 {
+    ObservableCollection<ClientPresenter> clients;
+    
+    private const int ItemsPerPage = 25;
+    private int currentPage = 0;
+    
     public MainWindow()
     {
         InitializeComponent();
+        using var context = new Demo3Context();
+        var dataSource = context.Clients.Select(client => new ClientPresenter
+        {
+            Id = client.Id,
+            Gendercode = client.Gendercode,
+            Firstname = client.Firstname,
+            Lastname = client.Lastname,
+            Patronymic = client.Patronymic,
+            Birthday = client.Birthday,
+            Phone = client.Phone,
+            Email = client.Email,
+            Registrationdate = client.Registrationdate,
+        });
+        clients = new ObservableCollection<ClientPresenter>(dataSource);
+        UpdateListBox();
+        
+        BackButton.IsEnabled = false;
     }
 
+    public class ClientPresenter() : Client
+    {
+        
+    }
+    
+    private void UpdateListBox()
+    {
+        var pagedClients = clients.Skip(currentPage * ItemsPerPage).Take(ItemsPerPage).ToList();
+        ClientsListBox.ItemsSource = pagedClients;
+        
+        BackButton.IsEnabled = currentPage > 0;
+        NextButton.IsEnabled = (currentPage + 1) * ItemsPerPage < clients.Count;
+    }
+    
+    private void Back_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (currentPage > 0)
+        {
+            currentPage--;
+            UpdateListBox();
+        }
+    }
+
+    private void Next_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if ((currentPage + 1) * ItemsPerPage < clients.Count)
+        {
+            currentPage++;
+            UpdateListBox();
+        }
+    }
+    
     private void AddClient_OnClick(object? sender, RoutedEventArgs e)
     {
         AddAndUpdate addAndUpdateButton = new AddAndUpdate();
